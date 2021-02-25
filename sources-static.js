78d561e3498da90bb50be6dd492d27fd7cc51470
Colby Fayock <fayock@gmail.com>
2021-02-25 16:11:39 -0500

    copy from content-reminder

---
+++ sources-static.js
@@ -0,0 +1,101 @@
+const static = [
+  
+  // Resources
+  
+  {
+    "title": "Jamstack Handbook",
+    "link": "https://jamstackhandbook.com/",
+    "source": {
+      "title": "Colby Fayock",
+      "link": "https://www.colbyfayock.com/"
+    }
+  },
+  {
+    "title": "50 Projects for React & the Static Web",
+    "link": "https://50reactprojects.com/",
+    "source": {
+      "title": "Colby Fayock",
+      "link": "https://www.colbyfayock.com/"
+    }
+  },
+  {
+    "title": "Journey to Dev",
+    "link": "https://journeyto.dev/",
+    "source": {
+      "title": "Colby Fayock",
+      "link": "https://www.colbyfayock.com/"
+    }
+  },
+  
+  // egghead.io Courses
+  
+  {
+    "title": "Create a New Github Action to Automate Code Tasks with Javascript",
+    "link": "https://egghead.io/playlists/create-a-new-github-action-to-automate-code-tasks-with-javascript-f1e9?af=atzgap",
+    "source": {
+      "title": "egghead.io",
+      "link": "https://egghead.io/instructors/colby-fayock?af=atzgap"
+    }
+  },
+  {
+    "title": "Create an eCommerce Store with Next.js and Stripe Checkout",
+    "link": "https://egghead.io/playlists/create-an-ecommerce-store-with-next-js-and-stripe-checkout-562c?af=atzgap",
+    "source": {
+      "title": "egghead.io",
+      "link": "https://egghead.io/instructors/colby-fayock?af=atzgap"
+    }
+  },
+  {
+    "title": "Quickly Create a New Company Website Managed by a CMS with TakeShape and Gatsby",
+    "link": "https://egghead.io/playlists/quickly-create-a-new-company-website-managed-by-a-cms-with-takeshape-and-gatsby-4e4d?af=atzgap",
+    "source": {
+      "title": "egghead.io",
+      "link": "https://egghead.io/instructors/colby-fayock?af=atzgap"
+    }
+  },
+  {
+    "title": "Source and Create Nodes with Data from an API in Gatsby to Create a Pokemon Pokedex",
+    "link": "https://egghead.io/playlists/source-and-create-nodes-with-data-from-an-api-in-gatsby-to-create-a-pokemon-pokedex-5e28?af=atzgap",
+    "source": {
+      "title": "egghead.io",
+      "link": "https://egghead.io/instructors/colby-fayock?af=atzgap"
+    }
+  },
+  {
+    "title": "Create, Manage, and Test API Requests with Postman",
+    "link": "https://egghead.io/playlists/create-manage-and-test-api-requests-with-postman-de1b?af=atzgap",
+    "source": {
+      "title": "egghead.io",
+      "link": "https://egghead.io/instructors/colby-fayock?af=atzgap"
+    }
+  },
+  
+  // External Articles
+  
+  {
+    "title": "New to Jamstack? Everything You Need to Know to Get Started",
+    "link": "https://snipcart.com/blog/jamstack",
+    "source": {
+      "title": "Snipcart",
+      "link": "https://snipcart.com/blog?author=7049"
+    }
+  },
+  {
+    "title": "E-Commerce for React Developers [w/ Next.js Shop Tutorial]",
+    "link": "https://snipcart.com/blog/react-ecommerce-tutorial",
+    "source": {
+      "title": "Snipcart",
+      "link": "https://snipcart.com/blog?author=7049"
+    }
+  },
+  {
+    "title": "Build a Content Management System for an E-commerce Store with Next.js and Sanity",
+    "link": "https://egghead.io/learn/ecommerce/build-a-content-management-system-for-an-e-commerce-store-with-nextjs-and-sanity?af=atzgap",
+    "source": {
+      "title": "egghead.io",
+      "link": "https://egghead.io/instructors/colby-fayock?af=atzgap"
+    }
+  }
+]
+
+module.exports = static;
